28288e902741e0789d86b53d8efa7b5d62deafe8
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-03-28 19:14:19 +0200

    resetting app : on définit les fonctions fondamentales d'abord, puis les routes

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,33 @@
-const express = require("express");
-require('express-async-errors');
-const path = require("path");
-const cors = require("cors");
-const mongoDB = require("./config/mongoDB");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoDB = require('./config/mongoDB');
 
-const { errorHandler } = require("./middleware/errorMiddleware");
 
-//connect to database
+//on importe ./config/mongoDB.js qui s'occupe de la connexion à la base de données MongoDB, puis on l'exécute.
 mongoDB();
 
 const app = express();
 
-
+//On utilise bodyParser pour parser les requêtes entrantes dans le corps de la requête.
+app.use(bodyParser.json());
+//CORS (Cross Origin Resource Sharing) est un mécanisme qui permet d'accéder à des ressources distantes.
 app.use(cors());
+//on peut aussi le parametrer comme ceci:
+//app.use(cors({origin: 'http://localhost:4200'}));
+
+//On définit les headersqui seront utilisés dans les requêtes HTTP.
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/images", express.static(path.join(__dirname, "images")));
-
-// Set our api routes
-app.use("/api/auth", require("./routes/authentication"));
-app.use("/api/sauces", require("./routes/sauces"));
-
-app.use(errorHandler);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+//server.js s'occupe de lancer le serveur et importe app.js qui lui s'occupe de lancer l'application et de gérer la logique interne de l'application. 
+
 const http = require("http");
-const dotenv = require("dotenv").config(".env");
+const dotenv = require("dotenv").config('.env')
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.HOST_PORT || 3000;
 const server = http.createServer(app);
-
 server.listen(port, () => {
-  console.log(`démarrage du serveur sur le port ${port}`);
+  console.log(`Démarage du serveur sur le port ${port}`);
 });
