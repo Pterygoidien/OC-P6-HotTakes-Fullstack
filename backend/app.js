e315cc5a68f880cde3a9336c712d6415da90c298
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-03-28 19:28:40 +0200

    on définit les routes et la structure

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const bodyParser = require('body-parser');
 const mongoDB = require('./config/mongoDB');
+const { errorMiddleware } = require('./middleware/errorMiddleware');
 
 
 //on importe ./config/mongoDB.js qui s'occupe de la connexion à la base de données MongoDB, puis on l'exécute.
@@ -10,8 +10,6 @@ mongoDB();
 
 const app = express();
 
-//On utilise bodyParser pour parser les requêtes entrantes dans le corps de la requête.
-app.use(bodyParser.json());
 //CORS (Cross Origin Resource Sharing) est un mécanisme qui permet d'accéder à des ressources distantes.
 app.use(cors());
 //on peut aussi le parametrer comme ceci:
@@ -29,5 +27,11 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//On importe les routes de sauces et des utilisateurs.
+app.use('/api/auth', require('./routes/auth.route'));
+
+//on importe le middleware erreur en dernier pour qu'il soit appelé en dernier.
+app.use(errorMiddleware);
 
 module.exports = app;
